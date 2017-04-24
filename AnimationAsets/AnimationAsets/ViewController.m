abbd4a912b4bf4dd1a19dfb6ec6b7336d6098535
徐丽 <xuli5@le.com>
2017-04-24 15:13:42 +0800

    no message

---
+++ AnimationAsets/AnimationAsets/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AnimationAsets
+//
+//  Created by 徐丽 on 17/4/24.
+//  Copyright © 2017年 徐丽. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ AnimationAsets/AnimationAsets/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AnimationAsets
+//
+//  Created by 徐丽 on 17/4/24.
+//  Copyright © 2017年 徐丽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
